4898d9cd1117b83fd963b658a681941647f6c2e0
destroyer91114 <yangxin.sz.ch@gmail.com>
2018-12-19 18:42:55 +1100

    test

--- BinaryGap.py
+++ BinaryGap.py
@@ -1,1 +1,17 @@
-print 'hello world'
+def solution(N):
+    binary = format(N, "b")
+    print (binary)
+    x = 0
+    y = 0
+    int_binary = int(binary)
+    for value in binary:
+        if value == '0':
+            x +=1
+        if value == '1':
+            if x > y:
+                y = x
+            x = 0
+        #print x
+    print('the y is: {}'.format(y))
+
+solution(1041)
